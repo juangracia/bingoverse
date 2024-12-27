dcf2e3e9e47682001ce4ad55f967538a75da8962
Juan Gracia <juan.gracia@santexgroup.com>
2024-12-27 03:12:41 -0300

    feature: adding swagger support

--- bingoverse-config/src/main/java/com/bingoverse/config/BingoVerseConfigApplication.java
+++ bingoverse-config/src/main/java/com/bingoverse/config/BingoVerseConfigApplication.java
@@ -3,7 +3,7 @@ package com.bingoverse.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.bingoverse.config")
 public class BingoVerseConfigApplication {
 
 	public static void main(String[] args) {
